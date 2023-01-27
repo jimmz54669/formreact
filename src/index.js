b0115562a33653b1e21aacb37c04d1f409a40038
jimmz54669 <jimmz54669@gmail.com>
2023-01-27 20:10:51 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/App.test.js 	modified:   src/index.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"
+
+
+function MyForm(){
+  const [inputs, setInput] = useState ({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInput (values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault ();
+    console.log(inputs);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Find your TrueLove</h1>
+      <p>Sign up to find your match</p>
+      <label>
+        <input type="text" name="firstname" placeholder='Enter your first name:' value={inputs.firstname || ""} onChange={handleChange} />
+      </label>
+      <label>
+        <input type="text" placeholder='Enter your last name:' name="lastname" value={inputs.lastname || ""} onChange={handleChange} />
+      </label>
+      <label>
+        <input type="email" placeholder='Enter your email:' name="email" value={inputs.email || ""} onChange={handleChange} />
+      </label>
+      <label>
+        <input type="number" placeholder='Enter your age:' name="age" value={inputs.age || ""} onChange={handleChange} />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render (<MyForm />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
